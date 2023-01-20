d7c94262594036a9aa97607e93dfa657c5b2e3a2
Tom <tomvercaut@gmail.com>
2023-01-20 18:53:51 +0100

    Replaced Boost Program Options with CLI11.
    As a result the commandline application no longer depends on compiled Boost libraries.

--- cli/internal/src/app_options_parser.cpp
+++ cli/internal/src/app_options_parser.cpp
@@ -1,61 +1,40 @@
 #include "app_options_parser.h"
 
-#include <boost/program_options.hpp>
+#include <CLI/CLI.hpp>
 #include <iostream>
-
-namespace po = boost::program_options;
 
 namespace rtp::connect::tools::analyser::cli {
 
-auto Usage(const po::options_description& desc, const std::string& app_name)
-    -> std::string {
-  std::stringstream ss{""};
-  ss << "Usage: ";
-  ss << app_name;
-  ss << " [options]\n";
-  ss << desc;
-  return ss.str();
-}
-
-void MissingRequiredArgument(const std::string& app_name,
-                             const po::options_description& desc,
-                             const std::string& argument) {
-  std::cerr << "Required input argument missing: " << argument << "\n\n";
-  const auto usage = Usage(desc, app_name);
-  std::cerr << usage << std::endl;
-  exit(1);
+void CliError(const CLI::ParseError& e) {
+  std::cerr << "An error was caught while parsing the input arguments.\n\n"
+            << "name: " << e.get_name() << "\n"
+            << "message: " << e.what() << "\n"
+            << "exit code: " << e.get_exit_code() << "\n\n";
+  exit(e.get_exit_code());
 }
 
 auto ParseAppOptions(const std::string& app_name, int argc, char** argv)
     -> AppOptions {
-  po::options_description options{"Options"};
-  options.add_options()("help,h", "Print application usage and help message")(
-      "input,i", po::value<std::string>(), "RTP input file")(
-      "output,o", po::value<std::string>(), "output file")(
-      "data-dir", po::value<std::string>(),
-      "data directory in which the machine profiles are stored");
+  CLI::App app(app_name);
+  AppOptions ao;
 
-  po::variables_map vm;
-  po::store(po::parse_command_line(argc, argv, options), vm);
-  po::notify(vm);
-  if (vm.contains("help")) {
-    const auto usage = Usage(options, app_name);
-    std::cout << usage << std::endl;
-    exit(0);
-  }
-  AppOptions ao{};
-  if (vm.contains("input")) {
-    ao.input_file = vm["input"].as<std::string>();
-  } else {
-    MissingRequiredArgument(app_name, options, "input");
-  }
-  if (vm.contains("output")) {
-    ao.output_file = vm["output"].as<std::string>();
-  } else {
-    ao.output_file = "";
-  }
-  if (vm.contains("data-dir")) {
-    ao.data_directory = vm["data-dir"].as<std::string>();
+  app.add_option("-i,--input", ao.input_file, "RTP input file")
+      ->required(true)
+      ->option_text("file");
+  //  app.add_option("-o,--output", ao.output_file, "Write results to a file.");
+  app.add_option("--data", ao.data_directory,
+                 "Data directory in which the machine profiles are stored.")
+      ->option_text("directory");
+
+  try {
+    app.parse(argc, argv);
+  } catch (const CLI::ParseError& e) {
+    if (e.get_name() == "CallForHelp") {
+      std::cout << app.help() << "\n";
+      exit(0);
+    } else {
+      CliError(e);
+    }
   }
   return ao;
 }
